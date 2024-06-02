9841b3026020e1fb26530bd807815af1f0203633
MarharytaSom <158225902+MarharytaSom@users.noreply.github.com>
2024-06-02 16:46:42 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,25 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
 function createBoxes(amount) {
   const boxesContainer = document.getElementById('boxes');
   boxesContainer.innerHTML = '';
   let size = 30;
-  for (let i = 0; i < amount; i++){
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     box.style.backgroundColor = getRandomHexColor();
-    box.style.width = '${size}px';
-    box.style.height = '{size}px';
-    boxesContainer.appendChild(box);
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    fragment.appendChild(box);
     size += 10;
-}
+  }
+
+  boxesContainer.appendChild(fragment);
 }
 
 function destroyBoxes() {
   const boxesContainer = document.getElementById('boxes');
   boxesContainer.innerHTML = '';
-
 }
 
 document.querySelector('[data-create]').addEventListener('click', function () {
@@ -30,10 +34,9 @@ document.querySelector('[data-create]').addEventListener('click', function () {
   if (amount >= 1 && amount <= 100) {
     createBoxes(amount);
     input.value = '';
-  }
-  else {
+  } else {
     alert('Please enter a number between 1 and 100');
   }
 });
-  
+
 document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
